863d9642aa903face9b493545bbf79e15635d4b6
arthurtuio <arthur.henrique.2709@gmail.com>
2021-01-02 15:27:29 -0300

    created a localhost rds conn

--- lib/postgres_connector.py
+++ lib/postgres_connector.py
@@ -1,6 +1,6 @@
 # Baseado no Postgres Hook
 import psycopg2
-from lib.credentials import gide_db_credentials
+from lib.credentials import gide_db_credentials, local_host_credentials
 
 
 class PostgresConnector:
@@ -16,3 +16,8 @@ class PostgresConnector:
     def connect_using_default_credentials():
         default_conn = gide_db_credentials()
         return psycopg2.connect(default_conn)
+
+    @staticmethod
+    def connect_using_localhost_credentials():
+        localhost_conn = local_host_credentials()
+        return psycopg2.connect(localhost_conn)
